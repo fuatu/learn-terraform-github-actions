fbbf7a01b262f935c7df98cfb628a8fe5cbefa99
Fuat Ulugay <fuat.ulugay@trilogy.com>
2023-03-27 13:20:19 +0300

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "imgtest"
+    organization = "testimg"
 
     workspaces {
       name = "learn-terraform-github-actions"
